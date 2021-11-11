573829a294c7874c574cee3cd8c39984009ae008
insomniKat <ru3ykat@gmail.com>
2021-11-10 20:41:36 -0500

    departments table and seeds

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO departments (id, dept_name)
+VALUES
+    (id, 'Receiving'),
+    (id, 'Outgoing'),
+    (id, 'Sales'),
+    (id, 'Management');
