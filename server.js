297c861a3108c9ed1c5e04681cb340d01def7b2f
insomniKat <ru3ykat@gmail.com>
2021-11-10 22:50:52 -0500

    db connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3009;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -10,6 +11,21 @@ app.get('/', (req, res) => {
     res.json({
         message: 'Hello Massachusetts!'
     });
+});
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'ru3ykat-mySQL',
+        database: 'employees'
+    },
+    console.log('Connected to the employees database.')
+);
+
+db.query(`SELECT * FROM departments`, (err, rows) => {
+    console.log(rows);
 });
 
 // Default response for any other request (Not Found)
