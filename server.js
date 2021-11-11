e170c450a2a28b7d211924c0b9d35cf73d6851f7
insomniKat <ru3ykat@gmail.com>
2021-11-11 00:21:22 -0500

    db queries

--- server.js
+++ server.js
@@ -24,9 +24,37 @@ const db = mysql.createConnection(
     console.log('Connected to the employees database.')
 );
 
-db.query(`SELECT * FROM departments`, (err, rows) => {
-    console.log(rows);
-});
+// GET a single department
+// db.query(`SELECT * FROM departments WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// Delete a department
+// db.query(`DELETE FROM departments WHERE  id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a department
+// const sql = `INSERT INTO departments (id, dept_name)
+//         VALUES (?,?)`;
+// const params = [1, 'Receiving'];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// db.query(`SELECT * FROM departments`, (err, rows) => {
+//     console.log(rows);
+// });
 
 // Default response for any other request (Not Found)
 // Will override all other requests! Put at END!
