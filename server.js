3bbf46f3ca66f39538cffcd44be3ea0247ff1445
insomniKat <ru3ykat@gmail.com>
2021-11-12 21:24:06 -0500

    all department queries pass

--- server.js
+++ server.js
@@ -25,35 +25,76 @@ const db = mysql.createConnection(
 );
 
 // GET a single department
-// db.query(`SELECT * FROM departments WHERE id = 1`, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// });
+app.get('/api/department/:id', (req, res) => {
+    const sql = `SELECT * FROM departments WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params,(err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
 
 // Delete a department
-// db.query(`DELETE FROM departments WHERE  id = ?`, 1, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+app.delete('/api/department/:id', (req, res) => {
+    const sql = `DELETE FROM departments WHERE ID = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Department not found'
+            });
+        } else {
+            res.json({
+                message: 'Successfully deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
 
 // Create a department
-// const sql = `INSERT INTO departments (id, dept_name)
-//         VALUES (?,?)`;
-// const params = [1, 'Receiving'];
+app.post('/api/department', ({ body }, res) => {
+    const sql = `INSERT INTO departments (dept_name)
+    VALUES (?)`;
+    const params = (body.dept_name);
 
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'New department was added successfully.',
+            data: body
+        });
+    });
+});
 
-db.query(`SELECT * FROM departments`, (err, rows) => {
-    console.log(rows);
+// Get all departments
+app.get('/api/departments', (req, res) => {
+    const sql = `SELECT * FROM departments`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({ 
+            message: 'success',
+            data: rows
+        });
+    });
 });
 
 // Default response for any other request (Not Found)
